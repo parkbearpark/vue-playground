b8d1c6187064a36e7290f59b11ddb42ae529d77c
yuma <yumai6205@gmail.com>
2021-07-16 17:54:57 +0900

    slot 例

---
+++ src/components/Child.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <span>これは<slot name="slot-1">child-slot-1</slot>です</span>
+    <span>これは<slot name="slot-2">child-slot-2</slot>です</span>
+  </div>
+</template>
---
+++ src/components/Parent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>slot-1のみ</h1>
+    <Child>
+      <template v-slot:slot-1>parent-slot-1</template>
+    </Child>
+    <h1>slot-2のみ</h1>
+    <Child>
+      <template v-slot:slot-2>parent-slot-2</template>
+    </Child>
+    <h1>slot-1, slot-2</h1>
+    <Child>
+      <template v-slot:slot-1>parent-slot-1</template>
+      <template v-slot:slot-2>parent-slot-2</template>
+    </Child>
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from '@vue/composition-api'
+  import Child from './Child.vue'
+
+  export default defineComponent({
+    components: {
+      Child,
+    },
+    setup() {},
+  })
+</script>
